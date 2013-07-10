2cb3299de92264c88659145c86eb343f7549b215
Natan <natanstartup@gmail.com>
2013-07-10 02:47:51 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-response.send(buf.toString();
+response.send(buf.toString());
  });
 
 
